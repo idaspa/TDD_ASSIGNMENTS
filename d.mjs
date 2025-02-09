6a83220334039d4eb5380f72d05740d9d933102d
idaspa <idahelenhal@msn.com>
2025-02-09 20:16:20 +0100

    done

--- d.mjs
+++ d.mjs
@@ -3,21 +3,23 @@ import test from "./test.mjs";
 
 
 function guessNumber(target, guess) {
-    if (typeof target !== "number" || typeof guess !== "number") { 
+    if (typeof target !== "number" || typeof guess !== "number") {
         return null;
     }
-    if (!Number.isInteger(target) || !Number.isInteger(guess)) { 
+    if (!Number.isInteger(target) || !Number.isInteger(guess)) {
         return null;
     }
     else if (guess > target) {
         return "Too high";
     }
-   else  if (guess < target) {
+    else if (guess < target) {
         return "Too low";
     }
-    else (guess === target) 
+    else {
+        (guess === target)
         return "Correct!";
     }
+}
 
 
 
@@ -29,7 +31,7 @@ tests.isEqual(guessNumber(10, 10), "Correct!", "If target is 10 and guess is 10,
 
 tests.isEqual(guessNumber(6, "6"), null, "If the guess is in a string return null.");
 tests.isEqual(guessNumber(3, 4.2), null, "If a guess is not a integer number return null.");
-tests.isEqual(guessNumber("9","8"), null, "If guess || target is a string it should return null")
+tests.isEqual(guessNumber("9", "8"), null, "If guess || target is a string it should return null")
 
 tests.isEqual(guessNumber(-8, -8), "Correct!", "If target is -8 and guess is -8 it should retun correct");
 tests.isEqual(guessNumber(-20, 15), "Too high", "If guess is 15 and target is -20 it should return too high");
